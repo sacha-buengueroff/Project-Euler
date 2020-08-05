a50e8cd7fe1f4074fcd363c9688c1f76d2ca9e9f
Sacha <69225351+sachabuengueroff@users.noreply.github.com>
2020-08-04 22:56:41 -0300

    Update Problem 3.py

--- Problem 3.py
+++ Problem 3.py
@@ -1,3 +1,9 @@
+"""
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+"""
+
 import math
 def isPrime(n):
     for i in range(2,int(n**0.5)+1):
