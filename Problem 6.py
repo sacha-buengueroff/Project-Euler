c166a4275c5b71ed4a0bc3339406b16ff394d152
Sacha <69225351+sachabuengueroff@users.noreply.github.com>
2020-08-04 23:07:05 -0300

    Update Problem 6.py

--- Problem 6.py
+++ Problem 6.py
@@ -1,3 +1,15 @@
+"""
+The sum of the squares of the first ten natural numbers is,
+
+The square of the sum of the first ten natural numbers is,
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is
+
+.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+"""
+
 def sumOfSquares(n):
     sum = 0
     for i in range(1,n+1):
