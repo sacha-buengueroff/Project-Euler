b59c881c2004ea07d79fd29f025080911759fd67
Sacha <69225351+sachabuengueroff@users.noreply.github.com>
2020-08-04 23:04:17 -0300

    Update Problem 36.py

--- Problem 36.py
+++ Problem 36.py
@@ -1,3 +1,11 @@
+"""
+The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+
+Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+
+(Please note that the palindromic number, in either base, may not include leading zeros.)
+"""
+
 def toBin(n):
     x = bin(n)
     x = str(x)
